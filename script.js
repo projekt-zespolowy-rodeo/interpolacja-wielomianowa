64e63308b8067832a3536ff5918f5e92f2089108
Kamil <kamil.gorny99@gmail.com>
2020-11-30 13:14:06 +0100

    Update polynomial after deletation

--- script.js
+++ script.js
@@ -6,6 +6,47 @@ let list = document.getElementById('tableBody')
 var data = []
 let polyniomaBox = document.getElementById('polyniomial')
 form.addEventListener('submit', createList);
+let input = document.querySelector('fileselector')
+
+function readSingleFile(e) {
+    document.getElementById("add").disabled = true;
+    var file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e){
+        var content = e.target.result;
+        displayContents(content);
+    };
+
+    reader.readAsText(file);
+
+  }
+
+  function displayContents(content){
+    var lines = content.split(/\r\n|\n/);
+
+    for (i=0; i<lines.length;i++){
+        var number = lines[i].split(" ");
+        var year = number[0];
+        var crime_number = number[1];
+        var deriv = number[2];
+        //console.log(number[2]);
+        //console.log(number[2].split(",").length)
+        //var numOfDiv = number[2].split(",").length;
+        //console.log(number[2].split(",")[1]);
+        //var deriv = [];
+        /*for(var each of number[2].split(",")){
+            deriv.push(each);
+        }*/
+        createListFromFile(year, crime_number, deriv);
+    }
+
+
+  }
+  
+document.getElementById('file-selector').addEventListener('change', readSingleFile, false);
 
 function deleteRow(o){
 
@@ -21,12 +62,48 @@ function deleteRow(o){
     }
 
     p.parentNode.removeChild(p);
-    console.log("Array after deletion: "+data)
+    console.log("Array after deletion: "+data) 
+    if (data.length == 0){
+        document.getElementById("add").disabled = false;
+    }
+    polyniomaBox.innerHTML="<h1>"+""+"</h1>"
+    interpolation(data)
+}
 
+function createListFromFile(year, crime_number, derivative){
+
+     if(year != '' && crime_number !=''){
+         let list_row = document.createElement('tr')
+         list_row.innerHTML="<td>"+year+"</td>"+"<td>"+crime_number+"</td>"+"<td>"+derivative+"</td>"+"<td>"+"<button type=\"button\" class=\"btn btn-danger\" onclick=\"deleteRow(this);\">Usuń</button>"+"</td>"
+         list.appendChild(list_row)
+         var temp_table =[]
+         if (derivative != ""){
+            temp_table=derivative.split(",")
+         }
+         var temp = []
+         if (temp_table.length != 0){   
+         for(var element of temp_table){
+         temp.push(parseInt(element))
+         }
+         data.push([parseInt(year), parseInt(crime_number), temp])
+         }
+         else{
+             data.push([parseInt(year), parseInt(crime_number)])
+         }
+ 
+         console.log("Added values: ["+year+", "+crime_number+", "+derivative+"]")
+         console.log("Array after addition: "+data);
+         interpolation(data)
+         
+     }
+ 
+     
+ 
+ 
 }
 
 function createList(e){
-e.preventDefault();
+    e.preventDefault()
 
    // console.log(typeof year.value)
     if(year != '' && crime_number !=''){
@@ -51,8 +128,7 @@ e.preventDefault();
         console.log("Added values: ["+year.value+", "+crime_number.value+", "+derivative.value+"]")
         console.log("Array after addition: "+data);
 
-        if(data.length==2)
-            interpolation(data)
+        interpolation(data)
         
     }
 
@@ -92,37 +168,13 @@ function interpolation(data){
 
 function display_status(b,t){
     j=0
+    napis=""
     for (var i of b){
+        napis=napis+"b["+ j +"] = "+i+", "
         console.log("b["+ j++ +"] = "+i)
     }
-    lista_znakow=[]
-    if ((b[0]-b[1]*t[0][0]+b[2]*t[0][0]*t[1][0]+b[3]*t[0][0]*t[2][0]-
-        b[3]*t[0][0]*t[1][0]*t[2][0])>= 0){
-        lista_znakow.push("+")
-    }
-    else{
-        lista_znakow.push("")
-    }
-    if (b[1]-b[1]*t[1][0]-b[2]*t[0][0]+b[3]*t[1][0]*t[2][0] >= 0){
-        lista_znakow.push("+")
-    }
-    else{
-        lista_znakow.push("")
-    }
-    if (b[2]-t[2][0]*b[3]-b[3]*t[1][0]>=0){
-        lista_znakow.push("+")
-    }
-    else{
-        lista_znakow.push("")
-    }
 
-    var polynomial  = "P(x) = "+(b[3])+"(x^3) "+lista_znakow[2]+" "+(b[2]-t[2][0]*b[3]-b[3]*t[1][0]-t[0][0]*b[3])+"(x^2) "+lista_znakow[1]+
-    " "+(b[1]-b[1]*t[1][0]-b[2]*t[0][0]+b[3]*t[1][0]*t[2][0])+"x "+lista_znakow[0]+" "+(b[0]-b[1]*t[0][0]+b[2]*t[0][0]*t[1][0]+b[3]*t[0][0]*t[2][0]-
-    b[3]*t[0][0]*t[1][0]*t[2][0])
-
-    printPolynomial(polynomial)
-
-
+    printPolynomial(napis)
 }
 
 
